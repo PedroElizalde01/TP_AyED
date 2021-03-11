f195628b76bcc1cfbba742576b8e7dce935717ad
Pedro Elizalde <pedro.elizalde@ing.austral.edu.ar>
2021-03-11 12:11:01 -0300

    Some Corrections

--- src/NumRom.java
+++ src/NumRom.java
@@ -1,3 +1,5 @@
+//@author - Timoteo Sanchez, Juan Pablo Senmartin, Pedro Elizalde
+
 import java.util.HashMap;
 import java.util.Scanner;
 
@@ -6,27 +8,26 @@ public class NumRom {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
-        System.out.println(romanToInt(input)); //Llamo al metodo que traduce de NumRomano a entero
+        System.out.println(romanToInt(input)); //Call method to translate a roman number to int, and then print
 
     }
     public static int romanToInt(String input) {
-        int result = 0;
-        if (input.length() != 1) {                                              // Si no es de una letra,
-            for (int i = 0; i < input.length() - 1; i++) {                      // para cada una,
-                    if (romanNumsInfo().get(input.charAt(i)) != null) {         // si es distinta a null,
-                        int num = romanNumsInfo().get(input.charAt(i));         // busco su valor numerico y
-                        if (num < romanNumsInfo().get(input.charAt(i + 1))) {   // me fijo si es menor al numero siguiente
-                            result -= num;                                      //
-                        } else {                                                //
-                            result += num;                                      //
+        int result = 0;                                                         // Create local variable to storage int's
+        if (input.length() != 1) {                                              // If more than one letter,
+            for (int i = 0; i < input.length() - 1; i++) {                      // for each one,
+                    if (romanNumsInfo().get(input.charAt(i)) != null) {         // if's not null,
+                        int num = romanNumsInfo().get(input.charAt(i));         // search for numeric value and
+                        if (num < romanNumsInfo().get(input.charAt(i + 1))) {   // check if it's smaller than the next number
+                            result -= num;                                      // If it is, subtract to result
+                        } else {                                                // if its bigger,
+                            result += num;                                      // add to result
                         }
                     }
             }
         }
-        result += romanNumsInfo().get(input.charAt(input.length()-1));
-        return result;
+        return result + romanNumsInfo().get(input.charAt(input.length() - 1));  // return int equivalent to roman number
     }
-    private static HashMap<Character, Integer> romanNumsInfo(){
+    private static HashMap<Character, Integer> romanNumsInfo(){ // This is where 'romanToInt()' gets the info to translate
         HashMap<Character,Integer> romanNums = new HashMap<>();
         romanNums.put('I',1);
         romanNums.put('V',5);

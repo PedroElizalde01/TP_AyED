2167e155a09d1aab28362d0c2dc912d0204c38a5
JuanPabloSenmartin <juanpablo.senmartin@ing.austral.edu.ar>
2021-03-15 20:59:30 -0300

    s

--- src/TP_ComparaciónAlgoritmos/Ejercicio2c.java
+++ src/TP_ComparaciónAlgoritmos/Ejercicio2c.java
@@ -6,15 +6,15 @@ public class Ejercicio2c {
         long currentTime= System.currentTimeMillis();
         Burbujeo.bubbleSort(arrayToSort);
         long bubbleTime= (System.currentTimeMillis()- currentTime);
-        System.out.println("El tiempo en segundos es de:" + bubbleTime + "milisegundos");
+        System.out.println("El tiempo en segundos es de:" + bubbleTime + " milisegundos");
         currentTime= System.currentTimeMillis();
         Selection.selectionSort(arrayToSort);
         long selectionTime= (System.currentTimeMillis()- currentTime);
-        System.out.println("El tiempo en segundos es de:" + selectionTime + "milisegundos");
+        System.out.println("El tiempo en segundos es de:" + selectionTime + " milisegundos");
         currentTime= System.currentTimeMillis();
         Insertion.insertionSort(arrayToSort);
         long insertionTime= (System.currentTimeMillis()- currentTime);
-        System.out.println("El tiempo en segundos es de:" + insertionTime + "milisegundos");
+        System.out.println("El tiempo en segundos es de:" + insertionTime + " milisegundos");
 
     }
     public static Integer[] randomArray(int index){

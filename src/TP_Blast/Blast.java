704154007c269d1ed092f5a663fcd21336b1310c
TimoteoSanchez <timoteo.sanchez@ing.austral.edu.ar>
2021-03-17 20:55:22 -0300

    Agregue texto y un exeption en blast

--- src/TP_Blast/Blast.java
+++ src/TP_Blast/Blast.java
@@ -1,8 +1,15 @@
 package TP_Blast;
 
+import java.util.Scanner;
+
 public class Blast {
 //@author - Timoteo Sanchez, Juan Pablo Senmartin, Pedro Elizalde
-    public static double blast(String reference, String query){
+    public static double blast(String reference, String query) throws IllegalArgumentException{
+        //Compares the query sequence to the reference sequence nucleotide by nucleotide adding 1 if it is the same
+        // nucleotide and at the end returning the percentage of similarity
+        if(reference.length()!=query.length()){
+            throw new IllegalArgumentException("The query and the reference don`t have the same length");
+        }
         char[] charReference = reference.toLowerCase().toCharArray();
         char[] charQuery = query.toLowerCase().toCharArray();
         double counter= 0;
@@ -16,9 +23,10 @@ public class Blast {
     }
 
     public static void main(String[] args) {
-        String a = "AATGCCA";
-        String b = "TATGAAT";
-        double percentage = blast(a,b);
+        String reference = "AAAGTGCTGATA";
+        Scanner scanner= new Scanner(System.in);
+        String query= scanner.nextLine();
+        double percentage = blast(reference,query);
         System.out.println(percentage+"%");
     }
 }

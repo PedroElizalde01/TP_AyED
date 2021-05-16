2d0e56e519fb6e6d78cc689ac8941177bc28dbde
JuanPabloSenmartin <juanpablo.senmartin@ing.austral.edu.ar>
2021-05-16 18:20:54 -0300

    recursividad arreglado

--- src/TPComparacióndeArboles/BlackAndRedTree.java
+++ src/TPComparacióndeArboles/BlackAndRedTree.java
@@ -1,6 +1,8 @@
 package TPComparacióndeArboles;
 
 
+import PilasyColas.StackNode;
+
 /**************************************
      Author/s: Timoteo Sanchez Varsallona, Juan Pablo Senmartin, Pedro Elizalde Acevedo
      Date: 20/04/2021
@@ -93,7 +95,7 @@ package TPComparacióndeArboles;
 
 
         // METODOS PRIVADOS
-        private NodoTriple<T> getMax(NodoTriple<T> t){
+        private NodoBnR<T> getMax(NodoBnR<T> t){
             if (t.der == null)
                 return t;
             else
@@ -165,4 +167,78 @@ package TPComparacióndeArboles;
                 t = t.der;
             return t;
         }
+        private boolean isEstable(StackNode<NodoBnR<T>> stackNode){
+            if (isSonRed(root) != null || mismaCantidadDeNodosNegros(root,0,0) == -1){
+                return false;
+            }
+            return true;
+        }
+
+        private int mismaCantidadDeNodosNegros(NodoBnR<T> node, int cantizq, int cantder){
+            if(node != null){
+                cantizq = mismaCantidadDeNodosNegros(node.izq,cantizq,cantder);
+                cantder = mismaCantidadDeNodosNegros(node.der,cantizq,cantder);
+                if (cantder != cantizq){
+                    return -1;
+                }
+            }
+            return 1;
+        }
+    private NodoBnR<T> isSonRed(NodoBnR<T> node) {
+        if (node != null) {
+            if ((node.izq.isRed && node.isRed) || (node.der.isRed && node.isRed)) {
+                return node;
+            } else {
+                if(isSonRed(node.izq)==null){
+                    return isSonRed(node.der);
+                }
+                return isSonRed(node.izq);
+            }
+        }
+        return null;
     }
+
+    private NodoBnR<T> rotateWithLeftChild( NodoBnR<T> k2 ){
+        NodoBnR<T> k1 = k2.izq;
+        k2.izq = k1.der;
+        k1.der = k2;
+        return k1;
+    }
+    private NodoBnR<T> rotateWithRightChild( NodoBnR<T> k1)
+    {
+        NodoBnR<T> k2 = k1.der;
+        k1.der = k2.izq;
+        k2.izq = k1;
+        return k2;
+    }
+    private NodoBnR<T> doubleWithLeftChild( NodoBnR<T> k3 )
+    {
+        k3.izq = rotateWithRightChild( k3.izq );
+        return rotateWithLeftChild( k3 );
+    }
+    private NodoBnR<T> doubleWithRightChild( NodoBnR<T> k1 )
+    {
+        k1.der = rotateWithLeftChild( k1.der );
+        return rotateWithRightChild( k1 );
+    }
+    private NodoBnR<T> changeColors(NodoBnR<T> k1){
+            k1.isRed=false;
+            return k1;
+    }
+
+    private StackNode<NodoBnR<T>> nodeJourney(NodoBnR<T> t, Comparable<T> x, StackNode<NodoBnR<T>> stack){
+        // Devuelve el camino que hace para insertar el nodo nuevo y lo inserta
+        if (t == null){
+            t = new NodoBnR<T>();
+            t.elem = (T) x;
+        }
+        else if (x.compareTo(t.elem) < 0) {
+            stack.stack(t);
+            stack = nodeJourney(t.izq, x, stack);
+        }else{
+            stack.stack(t);
+            stack = nodeJourney(t.der, x, stack);
+        }
+        return stack;
+    }
+}
